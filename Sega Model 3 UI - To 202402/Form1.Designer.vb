413b790bbf5b8a54ce024cfd0cb542e4aff758dd
BackPonBeauty <vf2_casino@yahoo.co.jp>
2024-09-11 22:13:56 +0900

    Changed the resolution selection method. Added FFB option.

--- Sega Model 3 UI - To 202402/Form1.Designer.vb
+++ Sega Model 3 UI - To 202402/Form1.Designer.vb
@@ -2695,7 +2695,7 @@ Partial Class Form1
         Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Sega Model 3 UI - Ver 1.1.1.5"
+        Me.Text = "Sega Model 3 UI - Ver 1.1.1.6"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
