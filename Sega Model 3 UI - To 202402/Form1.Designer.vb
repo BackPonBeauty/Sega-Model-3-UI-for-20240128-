175ff718a778eb186dd3e603790b25451218a363
masaya ishihara <vf2_casino@yahoo.co.jp>
2024-09-30 14:20:28 +0900

    20240930_1

--- Sega Model 3 UI - To 202402/Form1.Designer.vb
+++ Sega Model 3 UI - To 202402/Form1.Designer.vb
@@ -2755,7 +2755,7 @@ Partial Class Form1
         Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Sega Model 3 UI - Ver 1.1.1.6"
+        Me.Text = "Sega Model 3 UI - Ver 1.1.1.7"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
